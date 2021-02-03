cd5598961423c33d991e719b17c55bbdbb0e8fb5
thebearslicer <70990720+thebearslicer@users.noreply.github.com>
2021-02-03 18:22:33 -0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,53 @@
+//conect to html vars
+const moneyHeading = document.querySelector("#money-heading");
+const moneyBtn = document.querySelector("#money-btn");
+const hireTaxBtn = document.querySelector("#hireTax-btn")
+const hireTax10Btn = document.querySelector("#hireTax10-btn")
+const collecterHeading = document.querySelector("#collecter-heading");
+// game vars
+let money = 0;
+let taxCollecters = 0;
+let Hire1cost = 10;
+//Update UI
+const updateUI = () => {
+    moneyHeading.innerHTML = `Money: ${money}`;
+    collecterHeading.innerHTML = `Tax Collecters: ${taxCollecters}`;
+    hireTaxBtn.textContent = 'Hire tax collecter || ' + Hire1cost + ' money';
+    hireTax10Btn.textContent = 'Hire 10 tax collecters || ' + Hire1cost * 10 + ' money';
+}
+
+//event listener for collect money button
+moneyBtn.addEventListener("click", () => {
+    money += 1;
+    updateUI();
+    
+});
+//hire tax collecter
+hireTaxBtn.addEventListener("click", () => {
+    if (money >= Hire1cost) {
+        money -= Hire1cost;
+        taxCollecters += 1;
+        Hire1cost *= 1.2;
+        Hire1cost = Math.round(Hire1cost);
+    }
+});
+//hire 10 tax collecters
+hireTax10Btn.addEventListener("click", () => {
+    if (money >= Hire1cost * 10) {
+        money -= Hire1cost * 10;
+        taxCollecters += 10;
+        Hire1cost *= 1.2;
+        Hire1cost = Math.round(Hire1cost);
+    }
+});
+
+
+//game loop
+window.setInterval(() => {
+    money += taxCollecters;
+    
+}, 1000);
+window.setInterval(() => {
+    updateUI();
+    
+}, 1);
